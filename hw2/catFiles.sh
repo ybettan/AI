19901719c15450f4cc27e775a27492d87241ec67
Yoni Bettan <ybettan@redhat.com>
2017-12-25 20:18:44 +0200

    HW basically done, competition_player left
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- hw2/catFiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for f in temp/*; do
-    echo ----------$f-------------
-    cat $f
-done
--- hw2/runThreads.py
+++ hw2/runThreads.py
@@ -7,28 +7,6 @@ import shutil
 import time
 import sys
 
-#args = sys.argv[1:]
-#DEBUG = False
-#if args and args[0] == "--debug":
-#    #if args[0] == '--debug'
-#    DEBUG = True
-#
-#players = [ \
-#           'simple_player', \
-#           'better_player', \
-#           'min_max_player', \
-#           'alpha_beta_player' \
-#          ]
-#times = ['2', '10', '50']
-#num_of_games = 5
-#
-#if DEBUG:
-#    players = [ \
-#               'min_max_player', \
-#               'alpha_beta_player' \
-#              ]
-#    times = ['2']
-#    num_of_games = 1
 
 players = [ \
            'simple_player', \
@@ -39,48 +17,42 @@ players = [ \
 times = ['2', '10', '50']
 iterations = ['1', '2', '3', '4', '5']
 
+
 def callto(p1, p2):
 
     counter = 0
-    for time in times:
+    for t in times:
         for it in iterations:
 
             # p1 VS p2
             filename = "temp/{}_{}_time_{}_iteration_{}.txt".format(p1, p2, \
-                    time, it)
+                    t, it)
             file = open(filename, 'w+')
 
-            print('python run_game.py 2 {} 5 n {} {}'.format(time, p1, p2))
-            call(['python', 'run_game.py', '2', time, '5', 'n', p1, p2], stdout=file)
-            #call(['python', 'run_game.py', '2', time, '5', 'n', p1, p2])
+            start = time.time()
+            call(['python', 'run_game.py', '2', t, '5', 'n', p1, p2], stdout=file)
+            end = time.time()
+            print('python run_game.py 2 {} 5 n {} {}\t\t--> {} sec'.format( \
+                    t, p1, p2, end-start))
 
             file.close()
 
             # p2 VS p1
             filename = "temp/{}_{}_time_{}_iteration_{}.txt".format(p2, p1, \
-                    time, it)
+                    t, it)
             file = open(filename, 'w+')
 
-            print('python run_game.py 2 {} 5 n {} {}'.format(time, p2, p1))
-            call(['python', 'run_game.py', '2', time, '5', 'n', p2, p1], stdout=file)
-            #call(['python', 'run_game.py', '2', time, '5', 'n', p2, p1])
+            start = time.time()
+            call(['python', 'run_game.py', '2', t, '5', 'n', p2, p1], stdout=file)
+            end = time.time()
+            print('python run_game.py 2 {} 5 n {} {}\t\t--> {} sec'.format( \
+                    t, p2, p1, end-start))
 
             file.close()
 
             counter += 2
 
     assert(counter == 30)
-
-#def callto(time, p1, p2, iteration):
-#    file_name = 'temp/' + p1 + "_" + p2 + "_time_" + time + "_iteration_" + \
-#            iteration + '.txt'
-#
-#    file = open(file_name, 'w+')
-#
-#    print('python run_game.py 2 {} 5 n {} {}'.format(time, p1, p2))
-#    call(['python', 'run_game.py', '2', time, '5', 'n', p1, p2], stdout=file)
-#
-#    file.close()
 
 
 def run_threads():
@@ -97,23 +69,6 @@ def run_threads():
     assert(len(threads) == 6)
     for t in threads:
         t.join()
-
-#def run_threads():
-#    threads = []
-#
-#    for p1 in players:
-#        for p2 in players:
-#            if p1 == p2:
-#                continue
-#            for time in times:
-#                for i in range(num_of_games):
-#                    t = threading.Thread(target=callto, \
-#                            args=[time, p1, p2, str(i+1)])
-#                    threads.append(t)
-#                    t.start()
-#
-#    for t in threads:
-#        t.join()
 
 
 

11082f3f2fc0e9e4b80c9893d3b5a7b401bddc72
omrifro <33580856+omrifro@users.noreply.github.com>
2019-01-12 21:47:24 +0200

    Updated all of part B Added the 2_folds data files Added the experiments csv files

--- hw3/classifier.py
+++ hw3/classifier.py
@@ -1,7 +1,5 @@
-from hw3_utils import abstract_classifier, abstract_classifier_factory
-from hw3_utils import load_data
-#FIXME: this package needs to be remove
-import random
+from hw3_utils import abstract_classifier, abstract_classifier_factory, load_data
+from sklearn import tree, linear_model
 
 
 def euclidean_distance(x, y):
@@ -12,15 +10,14 @@ def euclidean_distance(x, y):
 
 class knn_classifier(abstract_classifier):
 
-    def __init__(self, data, tags, k):
-        self.data = data
-        self.tags = tags
+    def __init__(self, examples, k):
+        self.data = examples[0]
+        self.tags = examples[1]
         self.K = k
 
     def classify(self, features):
         k_dists, k_tags = [], []
         max_k_dist, max_k_idx = 0, 0
-        majority_call = True
         # Iterate through tagged data to find the K closest
         for neighbor_features, neighbor_tag in zip(self.data, self.tags):
             dist = euclidean_distance(features, neighbor_features)
@@ -28,16 +25,40 @@ class knn_classifier(abstract_classifier):
             if len(k_dists) < self.K:
                 k_dists.append(dist)
                 k_tags.append(neighbor_tag)
+                max_k_dist = max(k_dists)
+                max_k_idx = k_dists.index(max_k_dist)
             # we have already reached K samples so we replace the worst neighbore
             elif dist < max_k_dist:
                 k_dists[max_k_idx] = dist
                 k_tags[max_k_idx] = neighbor_tag
-            # calc the majority call - 0 in case of tie
-            max_k_dist = max(k_dists)
-            max_k_idx = k_dists.index(max_k_dist)
+                # calc the majority call - 0 in case of tie
+                max_k_dist = max(k_dists)
+                max_k_idx = k_dists.index(max_k_dist)
         # when done all examples
-        majority_call = round(sum(k_tags) / len(k_tags))
-        return majority_call
+        if len(k_tags) == 0:
+            return True
+        else:
+            return bool(round(sum(k_tags) / len(k_tags)))
+
+    def score(self, features, tags):
+        true_pos, true_neg, idx = 0, 0, 0
+        for smpl in features:
+            # get classification
+            tag = self.classify(smpl)
+            # get real value
+            real_tag = tags[idx]
+            if tag:
+                if real_tag:
+                    true_pos += 1
+                # else: false_pos += 1
+            else:
+                if not real_tag:
+                    true_neg += 1
+                # else: false_neg += 1
+            idx += 1
+
+        accuracy = (true_pos + true_neg) / len(features)
+        return accuracy
 
 
 class knn_factory(abstract_classifier_factory):
@@ -47,68 +68,95 @@ class knn_factory(abstract_classifier_factory):
         self.K = k
 
     def train(self, data, labels):
-        return knn_classifier(data, labels, self.K)
+        return knn_classifier((data, labels), self.K)
 
 
-#FIXME: this needs to be updated, also remove random module here - it is not
-# allowed, look at the mail to see dataset type (it is a tuple (features, labels))
+class tree_factory(abstract_classifier_factory):
+
+    def train(self, data, labels):
+        tree_classifier = tree.DecisionTreeClassifier(criterion='entropy')
+        tree_classifier.fit(data, labels)
+        return tree_classifier
+
+
+class perceptron_factory(abstract_classifier_factory):
+
+    def train(self, data, labels):
+        linear_classifier = linear_model.Perceptron(penalty=None, tol=1e-4, fit_intercept=True)
+        linear_classifier.fit(data, labels)
+        return linear_classifier
+
+
 def split_crosscheck_groups(dataset, num_folds):
-    true_exmp, false_exmp = [], []
-    # split the data to True and False examples
-    for example in dataset:
-        if example[-1]:
-            true_exmp.append(example)
-        else:
-            false_exmp.append(example)
-
+    true_exmp = dataset[0][dataset[1]]
+    false_exmp = [exmp for exmp in dataset[0] if exmp not in true_exmp]
     # divide into separate files
     true_sample_size = int(len(true_exmp)/num_folds)
     false_sample_size = int(len(false_exmp)/num_folds)
     for fold_i in range(num_folds):
         #FIXME: what happens if dataset % num_folds != 0, do we miss some at the last group ?
-        true_i = random.sample(true_exmp, true_sample_size)
-        false_i = random.sample(false_exmp, false_sample_size)
-        with open('data/ecg_fold_'+str(fold_i+1)+'.data', 'w') as f:
-            f.write(', '.join([str(feature) for feature in true_i]) + '\n')
-            f.write(', '.join([str(feature) for feature in false_i]) + '\n')
-        # remove all chosen from the picking list
-        true_exmp = [exmp for exmp in true_exmp if exmp not in true_i]
-        false_exmp = [exmp for exmp in false_exmp if exmp not in false_i]
+        true_i = true_exmp[true_sample_size*fold_i:true_sample_size*(fold_i + 1), :]
+        false_i = false_exmp[false_sample_size*fold_i:false_sample_size*(fold_i+1)][:]
+        with open('ecg_fold_'+str(fold_i+1)+'.data', 'w') as f:
+            for i in range(true_sample_size):
+                f.write(', '.join(map(str, true_i[i][:])) + ', 1\n')
+            for i in range(false_sample_size):
+                f.write(', '.join(map(str, false_i[i][:])) + ', 0\n')
 
-#def evaluate(classifier_factory, k):
-#
-#    # create training and test sets
-#    train_set = # all data files but data/ecg_fold_i.data
-#    test_set = # data/ecg_fold_i.data
-#    for i in range(1, k+1):
-#        # update train_set and test set
-#
-#    classifier = classifier_factory.train(train_set.data, train_set.labels)
-#
-#    # compute accuracy and error
-#    for features in test_set:
-#
-#        # get classification
-#        tag = classifier.classify(features)
-#
-#        # get real tag
-#
-#        real_tag
-#
-#        if classification is correct (check original full file)
-#          # if is positive
-#              # true_positive += 1
-#          # else
-#              # true_negative += 1
-#        elif classification is wrong
-#          # if is positive
-#              # false_positive += 1
-#          # else
-#              # false_negative += 1
 
+def load_k_fold_data(fold_idx):
+    lines = [line.strip() for line in open('ecg_fold_' + str(fold_idx) + '.data', 'r')]
+    train_i, labels_i = [], []
+    for line in lines:
+        b = [float(v) for v in line.split(', ')]
+        train_i.append(b[:len(b)-1])
+        labels_i.append(bool(b[-1]))
+    return train_i, labels_i
+
+
+def evaluate(classifier_factory, k):
+    Accuracy = 0
+    # create training and test sets for the i'th fold
+    full_train_data, full_train_tags, _ = load_data()
+    full_train_set = [list(l) for l in full_train_data]
+    for fold in range(k):
+        fold_test_set = load_k_fold_data(fold+1)
+        fold_train_set = [smpl for smpl in full_train_set if smpl not in fold_test_set[0]]
+        fold_train_tags = [full_train_tags[full_train_set.index(smpl)] for smpl in fold_train_set]
+        classifier = classifier_factory.train(fold_train_set, fold_train_tags)
+
+        # compute accuracy and error
+        Accuracy += classifier.score(fold_test_set[0], fold_test_set[1])
+
+    Accuracy /= k
+    Error = 1 - Accuracy
+    return Accuracy, Error
 
 
 train_set, train_tags, test_set = load_data()
-F = knn_factory(9)
-A = F.train(train_set, train_tags)
+split_crosscheck_groups((train_set, train_tags), 2)
+
+with open('experiment6.csv', 'w') as f:
+    k_vals, acc_list, err_list = [1, 3, 5, 7, 13], [], []
+    for k in k_vals:
+        knn = knn_factory(k)
+        acc, err = evaluate(knn, 2)
+        f.write(', '.join([str(k), str(acc), str(err)]) + '\n')
+        acc_list.append(acc)
+        err_list.append(err_list)
+
+
+tf = tree_factory()
+tree_acc, tree_err = evaluate(tf, 2)
+
+pf = perceptron_factory()
+percp_acc, percp_err = evaluate(pf, 2)
+
+with open('experiment12.csv', 'w') as f:
+    f.write(', '.join([str(1), str(tree_acc), str(tree_err)]) + '\n')
+    f.write(', '.join([str(2), str(percp_acc), str(percp_err)]) + '\n')
+
+
+import plot_results
+plot_results.plot_knn_accuracy(k_vals, acc_list)
 
